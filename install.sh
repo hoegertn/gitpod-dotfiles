3d9cd25c5d98f02f7da276ec54d1d3e52d292b2c
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2022-01-23 23:23:06 +0100

    feat: maven settings

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+mkdir -p ~/.m2
+
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/bash ~/.bashrc.d/90-hoegertn
+ln -sf ~/.dotfiles/maven-settings.xml ~/.m2/settings.xml
